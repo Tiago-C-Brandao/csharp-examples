9626c806d63a86951981b87b3b251f56cd3e3815
Tiago C. Brandão <tiago_curvelo@hotmail.com>
2021-01-10 02:10:31 -0300

    adicionando struct e configurando parâmetros

---
+++ Aluno.cs
@@ -0,0 +1,8 @@
+namespace csharp_examples
+{
+    public struct Aluno
+    {
+       public string Nome { get; set; }
+       public decimal Nota { get; set; } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
 ﻿using System;
 
-namespace cshhrap_examples
+namespace csharp_examples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("X - Sair");
-            Console.WriteLine();
-
-            string opcaoUsuario = Console.ReadLine();
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0; 
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
-                switch(opcaoUsuario)
+                switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: Adicionar alunos
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome =  Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+                        
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
-                        //TODO: Listar alunos
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
                         //TODO: Aclcular média geral
@@ -32,7 +50,25 @@ namespace cshhrap_examples
                         throw new ArgumentOutOfRangeException();
 
                 }
+
+                opcaoUsuario = ObterOpcaoUsuario();
             }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+
+            return opcaoUsuario;
         }
     }
 }
